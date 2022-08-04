b95db5888c2071216f74ffed47802f74d430dfdf
Francisco Rotundo <francorotundo90@gmail.com>
2022-08-03 20:03:28 -0400

    Creación de barra de navegación y primera sección inicio.

---
+++ js/style.js
@@ -0,0 +1,11 @@
+// ------- menu responsive -------
+
+var sidemenu = document.getElementById("sidemenu");
+
+function openmenu(){
+    sidemenu.style.right="0";
+}
+
+function closemenu(){
+    sidemenu.style.right = "-150px";
+}
