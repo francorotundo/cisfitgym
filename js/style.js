2dd54a5922c771ab67301b02d87baa156583363d
Francisco Rotundo <francorotundo90@gmail.com>
2022-08-04 19:18:27 -0400

    Creación de seccion de bienvenida

--- js/style.js
+++ js/style.js
@@ -9,3 +9,30 @@ function openmenu(){
 function closemenu(){
     sidemenu.style.right = "-150px";
 }
+
+addEventListener("DOMContentLoaded", () => {
+    const sections = document.querySelectorAll(".section")
+    const menuitem = document.querySelectorAll(".menu_item")
+
+    const functionObserver = entries => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                const itemActual = Array.from(menuitem).find(item => item.dataset.url === entry.target.id)
+                itemActual.classList.add("active")
+                for (const item of menuitem) {
+                    if (item != itemActual) {
+                        item.classList.remove("active")
+                    }
+                }
+            }
+        })
+    }
+
+    const observer = new IntersectionObserver(functionObserver, {
+        root: null,
+        rootMargin: "0px",
+        threshold: 0.8
+    })
+
+    sections.forEach(section => observer.observe(section))
+})
